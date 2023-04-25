e22c4b8515d4cf30e0e05c347bbd0deb60809a26
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-04-25 10:03:49 -0600

    can now search and display star wars characters

--- src/screens/DetailScreen.jsx
+++ src/screens/DetailScreen.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const DetailScreen = () => {
-  return (
-    <div>DetailScreen</div>
-  )
-}
+  const [person, setPerson] = useState({});
+  const { id } = useParams();
 
-export default DetailScreen
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people/${id}`)
+      .then((res) => {
+        setPerson(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>{person.name}</h1>
+      <h2>Height: {person.height}cm</h2>
+      <h2>Weight: {person.mass}kg</h2>
+      <h2>Hair: {person.hair_color}</h2>
+    </div>
+  );
+};
+
+export default DetailScreen;
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchScreen = () => {
+  const navigate = useNavigate()
+  const [inputNum, setInputNum] = useState(null)
+
+  const changeNum = (e) => {
+    setInputNum(e.target.value)
+  }
+
+  const handleSearch = () => {
+    let num = +inputNum
+    if(num){
+      navigate(`/details/${num}`)
+    }else{
+      alert('Thats not a number')
+    }
+  }
+
   return (
-    <div>SearchScreen</div>
+    <div>
+      <h1>Search</h1>
+      <input type='text' placeholder='Type a number!' onChange={changeNum}/>
+      <button onClick={handleSearch}>Search</button>
+    </div>
   )
 }
 
